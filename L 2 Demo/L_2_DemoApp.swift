50fd9609ff78d90a7abd72389c868178f28e5f5d
sarath0761 <sarathsurya212@gmail.com>
2024-10-30 11:30:42 -0500

    Initial Commit

---
+++ L 2 Demo/L_2_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L_2_DemoApp.swift
+//  L 2 Demo
+//
+//  Created by SARATH SURYA SUNKARA on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct L_2_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
